61ca9c02b3f59a9acdf76754bb1d95f5c1516c39
Andrew Lockwood <andrew.lockwood578@myci.csuci.edu>
2023-06-03 16:25:59 -0700

    v2

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
+#Conway's Game of Life
+# The universe of the Game of Life is an infinite two-dimensional orthogonal grid of square cells,
+# each of which is in one of two possible states, alive or dead, or "populated" or "unpopulated".
+# Every cell interacts with its eight neighbours, which are the cells that are horizontally, vertically,
+# or diagonally adjacent. At each step in time, the following transitions occur:
+#
+# Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
+#
+# Any live cell with two or three live neighbours lives on to the next generation.
+# 
+# Any live cell with more than three live neighbours dies, as if by overpopulation.
+# 
+# Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+#
+# The initial pattern constitutes the seed of the system. The first generation is created by applying
+# the above rules simultaneously to every cell in the seed—births and deaths occur simultaneously, and
+# the discrete moment at which this happens is sometimes called a tick (in other words, each generation
+# is a pure function of the preceding one). The rules continue to be applied repeatedly to create further
+# generations.
+# Author: Andrew Lockwood
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -40,18 +60,19 @@ def draw(grid):
     plt.draw()
 
 def main():
-    grid = Grid(size=300, n=3500)  # Create a 100x100 grid with 500 random living cells
+    grid = Grid(size=300, n=45000)  # Create a 300x300 grid with 45000 random living cells
     plt.ion()
+    draw(grid)  # Draw the initial grid
+    input("Press Enter to start the simulation...")
     try:
         while True:
-            draw(grid)
             grid.next_state()
+            draw(grid)
             plt.pause(0.05)  # Update more quickly to see the evolution
             plt.clf()
     except KeyboardInterrupt:
         print("Interrupted!")
         plt.close('all')
-
 
 if __name__ == '__main__':
     main()
